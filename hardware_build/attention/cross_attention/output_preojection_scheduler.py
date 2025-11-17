724d49b52fdffbeed564e2379859f3b0c3323ec1
Ezra Reiss <ezrareiss1@gmail.com>
2025-11-17 12:54:19 -0500

    added the output projection in cross attention

--- hardware_build/attention/cross_attention/output_preojection_scheduler.py
+++ hardware_build/attention/cross_attention/output_preojection_scheduler.py
@@ -2,7 +2,7 @@ import allo
 from allo.ir.types import float32, bfloat16, int32, int16, int8, int4
 import numpy as np
 from datetime import datetime
-import matmul_two as matmul_two
+import output_projection as output_projection
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).resolve().parents[2]))
@@ -19,17 +19,17 @@ def schedule_baseline_randomized_matmul_two(
     Input matrix Q is of shape (A_L, H_D) and K is of shape (V_L, H_D)
     """
     type_str = str(str(N_T).split(".")[-1])[:-2]
-    A_L = CAC.LENGTH_OF_ACTION_CHUNK
-    H_D = CAC.HEAD_DIM
-    V_L = CAC.DEFAULT_Tf
-    S = np.random.randn(A_L, V_L).astype(N_T) #input matrix S is of shape (A_L, V_L) (output of softmax)
-    V = np.random.randn(V_L, H_D).astype(N_T) #input matrix V is of shape (V_L, H_D) (VLM)
-    Z = np.zeros((A_L, H_D), dtype=N_T)
-    s = allo.customize(matmul_two.matmul_two, instantiate=[A_T, A_L, V_L, H_D])
-    name = f"matmul_two_{A_L}_{V_L}_{H_D}_{type_str}_{mode}_baseline_{datetime.now().strftime("%Y%m%d_%H%M%S")}.prj"
+    A_L = CAC.LENGTH_OF_ACTION_CHUNK #Action length
+    A_D = CAC.ACTION_HIDDEN_SIZE #Action output dimension
+    V_D = CAC.Q_PROJ_OUT_DIM #This is the VLM output dimension
+    Z = np.random.randn(A_L, V_D).astype(N_T) #input matrix Z is of shape (A_L, V_D) (output of matmul two and fused between all the heads)
+    O = np.random.randn(V_D, A_D).astype(N_T) #input matrix O is of shape (V_D, A_D) (output projection matrix)
+    Z_NEW = np.zeros((A_L, A_D), dtype=N_T) #output matrix is of shape (A_L, A_D) (output of output projection) 
+    s = allo.customize(output_projection.output_projection, instantiate=[A_T, A_L, V_D, A_D])
+    name = f"output_projection_{A_L}_{V_D}_{A_D}_{type_str}_{mode}_baseline_{datetime.now().strftime("%Y%m%d_%H%M%S")}.prj"
     if mode == "llvm":
         s_llvm = s.build()
-        s_llvm(S, V, Z)
+        s_llvm(Z, O, Z_NEW)
         return Z, s
     elif mode == "csyn":
         s_csyn = s.build(target="vitis_hls", mode="csyn", project=name)
--- hardware_build/attention/cross_attention/output_projection.py
+++ hardware_build/attention/cross_attention/output_projection.py
@@ -6,14 +6,14 @@ sys.path.append(str(Path(__file__).resolve().parents[2]))
 from matrix_multiplies import mm1
 
 
-def matmul_two[
+def matmul_output_projection[
     T: (bfloat16, float32),
     A_L: int16, #action length
-    V_L: int16, #VLM length
-    H_D: int16 #Action single head dimension (1/12 of VLM dimension)
+    V_D: int16, #VLM embedded dimension
+    A_D: int16 #Action output dimension
 ](
-    S: "T[A_L, V_L]",
-    V: "T[V_L, H_D]",
-    Z: "T[A_L, H_D]"
+    Z: "T[A_L, V_D]",
+    O: "T[V_D, A_D]",
+    Z_NEW: "T[A_L, A_D]"
 ):
-    mm1[T, A_L, V_L, H_D](S, V, Z)
+    mm1[T, A_L, V_D, A_D](Z, O, Z_NEW)
